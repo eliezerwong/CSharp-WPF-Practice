95090b32ff3ca1c01b9bdc71bc5b4121d4253074
eliezerwong <70494072+eliezerwong@users.noreply.github.com>
2021-11-12 21:50:32 -0600

    Add files via upload

---
+++ PracticeClasses/PracticeClasses/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticeClasses
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            bool isValid = true;
+            
+            if (string.IsNullOrWhiteSpace(txtManu.Text) == true)
+            {
+                isValid = false;
+                MessageBox.Show("Enter valid Manu:");
+            }
+
+            if (string.IsNullOrWhiteSpace(txtName.Text) == true)
+            {
+                isValid = false;
+                MessageBox.Show("Enter valid Name:");
+            }
+
+            double inputPrice = 0;
+            if (double.TryParse(txtPrice.Text, out inputPrice) == false)
+            {
+                isValid = false;
+                MessageBox.Show("Enter valid Manu:");
+            }
+
+            if (string.IsNullOrWhiteSpace(txtImage.Text) == true)
+            {
+                isValid = false;
+                MessageBox.Show("Enter valid Image:");
+            }
+
+            if (isValid == false)
+            {
+                return;
+            }
+
+            Toy toy = new Toy() { Manufacturer = txtManu.Text, Name = txtName.Text, Price = inputPrice, Image = txtImage.Text };
+            
+            lstToys.Items.Add(toy);
+        }
+
+        private void lstToys_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Toy selectedToy = (Toy)lstToys.SelectedItem;
+            MessageBox.Show(selectedToy.GetAisle());
+
+            imgImage.Source = new BitmapImage(new Uri(selectedToy.Image));
+        }
+    }
+}
---
+++ PracticeClasses/PracticeClasses/Toy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeClasses
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        private string Aisle;
+
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Image = string.Empty;
+            Aisle = string.Empty;
+        }
+
+        public string GetAisle()
+        {
+            string aisle = Manufacturer.Substring(0, 1) + Convert.ToString(Price).Replace(".", "");
+            return aisle;
+        }
+
+        public override string ToString()
+        {
+            return $"{Manufacturer} - {Name}";
+        }
+    }
+}
