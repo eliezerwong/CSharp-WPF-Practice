cb8527e1c38a4f04b42d57c95d6d6509ba262643
eliezerwong <70494072+eliezerwong@users.noreply.github.com>
2021-11-12 21:53:21 -0600

    Add files via upload

---
+++ PracticeContact2/PracticeContact2/Contact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeContact2
+{
+    public class Contact
+    {
+        //Id|FirstName|LastName|Email|Photo
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Photo { get; set; }
+
+
+        public Contact()
+        {
+            Id = string.Empty;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+            Photo = string.Empty;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName}"; 
+        }
+    }
+}
---
+++ PracticeContact2/PracticeContact2/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticeContact2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var lines = File.ReadAllLines("contacts.txt").Skip(1);
+
+            foreach (var line in lines)
+            {
+                var pieces = line.Split('|');
+                Contact c = new Contact() { Id = pieces[0], FirstName = pieces[1], LastName = pieces[2], Email = pieces[3], Photo = pieces[4]};
+
+                lstContacts.Items.Add(c);
+            }
+        }
+
+        private void lstContacts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Contact selectedC = (Contact)lstContacts.SelectedItem;
+            txtFName.Text = selectedC.FirstName;
+            txtLName.Text = selectedC.LastName;
+            txtEmail.Text = selectedC.Email;
+
+            imgPhoto.Source = new BitmapImage(new Uri(selectedC.Photo));
+        }
+    }
+}
