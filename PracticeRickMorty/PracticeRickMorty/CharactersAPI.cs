79f9e2f758e13c5be951b61c2eed2e6bc77e4b81
eliezerwong <70494072+eliezerwong@users.noreply.github.com>
2021-11-12 21:58:13 -0600

    Add files via upload

---
+++ PracticeRickMorty/PracticeRickMorty/CharactersAPI.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeRickMorty
+{
+    public class CharactersAPI
+    {
+        public Info info { get; set; }
+        public List<Result> results { get; set; }
+    }
+
+    public class Result
+    {
+        public string name { get; set; }
+        public string image { get; set; }
+
+        public override string ToString()
+        {
+            return name; 
+        }
+    }
+
+    public class Info
+    {
+        public string count { get; set; }
+        public string pages { get; set; }
+        public string next { get; set; }
+        public string prev { get; set; }
+    }
+}
---
+++ PracticeRickMorty/PracticeRickMorty/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace PracticeRickMorty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string url = $"https://rickandmortyapi.com/api/character/?page=1";
+
+            using (var client = new HttpClient())
+            {
+                while (url != null)
+                {
+                    string jsonData = client.GetStringAsync(url).Result;
+                    CharactersAPI api = JsonConvert.DeserializeObject<CharactersAPI>(jsonData);
+
+                    foreach (var item in api.results)
+                    {
+                        lstCharacters.Items.Add(item);
+                    }
+
+                    url = api.info.next;
+                }
+            }
+        }
+
+        private void lstCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Result selectedChara = (Result)lstCharacters.SelectedItem;
+            txtName.Text = selectedChara.name;
+            imgPhoto.Source = new BitmapImage(new Uri(selectedChara.image));
+        }
+    }
+}
