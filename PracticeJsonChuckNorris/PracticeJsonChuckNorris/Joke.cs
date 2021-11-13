76f4a26bca8bb3c24f75fa37de8eb3a4527094ad
eliezerwong <70494072+eliezerwong@users.noreply.github.com>
2021-11-12 21:56:43 -0600

    Add files via upload

---
+++ PracticeJsonChuckNorris/PracticeJsonChuckNorris/Joke.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeJsonChuckNorris
+{
+    public class Joke
+    {
+        //     "categories":[]
+        //"created_at":"2020-01-05 13:42:21.795084",
+        //"icon_url":"https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+        //"id":"OWBACGXbTguTB73iN_fyqw",
+        //"updated_at":"2020-01-05 13:42:21.795084",
+        //"url":"https://api.chucknorris.io/jokes/OWBACGXbTguTB73iN_fyqw",
+        //"value":"The reason why TV shows get interrupted with emergency broadcasts is because Chuck Norris is interrupting the show."
+
+        public List<string> categories { get; set; }
+        public string created_at { get; set; }
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string updated_at { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+
+    }
+}
---
+++ PracticeJsonChuckNorris/PracticeJsonChuckNorris/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace PracticeJsonChuckNorris
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cboCategory.Items.Add("All");
+            cboCategory.SelectedIndex = 0;
+
+            using (var client = new HttpClient())
+            {
+                string catURL = "https://api.chucknorris.io/jokes/categories";
+                
+                string json = client.GetStringAsync(catURL).Result;
+                var results = JsonConvert.DeserializeObject<List<string>>(json);
+
+                foreach (var item in results)
+                {
+                    cboCategory.Items.Add(item);
+                }
+            }
+        }
+
+        private void btnGet_Click(object sender, RoutedEventArgs e)
+        {
+            string category = cboCategory.SelectedItem.ToString();
+            string jokeURL = $"https://api.chucknorris.io/jokes/random?category={category}";
+
+
+            using (var client = new HttpClient())
+            {
+                var joke = JsonConvert.DeserializeObject<Joke>(client.GetStringAsync(jokeURL).Result);
+
+                txtJoke.Text = joke.value;
+            }
+        }
+    }
+}
